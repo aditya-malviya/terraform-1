81fd1ada2669d0cb6edfad19885fa9eaaece3826
aditya <aditya>
2021-05-23 14:25:06 +0530

    added basic terraform projecr for practise

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "MyFirstInstnace" {
+  count         = 1
+  ami           = "ami-0353c615ae895754c"
+  instance_type = "t2.micro"
+
+  key_name = "aws"
+  tags = {
+    Name = "demoinstnce"
+  }
+}
