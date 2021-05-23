9dd640ee5f06aa9fd226675b3858feb58c434729
pranaynimkar7@gmail.com <pranaynimkar7@gmail.com>
2021-05-23 15:32:48 +0530

    mycommit

--- main.tf
+++ main.tf
@@ -8,4 +8,13 @@ resource "aws_instance" "MyFirstInstnace" {
     Name = "demoinstnce"
   }
 }
+resource "aws_instance" "MySecondInstance" {
+  count         = 1
+  ami           = "ami-0353c615ae895754c"
+  instance_type = "t2.medium"
 
+  key_name = "aws"
+  tags = {
+    Name = "myinstance"
+  }
+}
